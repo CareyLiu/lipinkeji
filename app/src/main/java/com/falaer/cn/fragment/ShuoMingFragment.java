17d85d806ffbb4e236c1f1b55f69cd04969ec336
严相和 <yanxianghe1992@qq.com>
2021-09-17 13:41:15 +0800

    1.法拉尔说明书修改

--- app/src/main/java/com/falaer/cn/fragment/ShuoMingFragment.java
+++ app/src/main/java/com/falaer/cn/fragment/ShuoMingFragment.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import com.falaer.cn.R;
 import com.falaer.cn.app.BaseActivity;
 import com.falaer.cn.basicmvp.BaseFragment;
+import com.gyf.barlibrary.ImmersionBar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -87,7 +88,18 @@ public class ShuoMingFragment extends BaseFragment {
     public static ShuoMingFragment newInstance() {
         Bundle args = new Bundle();
         ShuoMingFragment fragment = new ShuoMingFragment();
-        fragment.setArguments(args);
         return fragment;
     }
+
+    @Override
+    protected void immersionInit(ImmersionBar mImmersionBar) {
+        mImmersionBar.with(this).fitsSystemWindows(true).statusBarColor(R.color.shenlanhei).init();
+    }
+
+
+    @Override
+    protected boolean immersionEnabled() {
+        return true;
+    }
+
 }
